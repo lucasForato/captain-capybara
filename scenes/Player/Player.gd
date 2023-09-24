21ffded1b1acabcaac50843285921e20860639ff
= <lucas7forato@gmail.com>
2023-09-24 17:21:13 -0300

    feat: adjust sound

--- scenes/Player/Player.gd
+++ scenes/Player/Player.gd
@@ -4,24 +4,20 @@ extends 'res://scripts/Pawn.gd'
 
 
 func _input(event: InputEvent):
-	var audio = self.get_children()[1]
 	var event_name: String
 	if event.is_action_pressed("move_down"):
-		audio.play()
 		event_name = "move_down"
 	if event.is_action_pressed("move_up"):
-		audio.play()
 		event_name = "move_up"
 	if event.is_action_pressed("move_left"):
-		audio.play()
 		event_name = "move_left"
 	if event.is_action_pressed("move_right"):
-		audio.play()
 		event_name = "move_right"
 	
 	if event_name:
 		var can_move = Grid.request_move(self, event_name)
 		if not can_move:
 			return
+		$AudioStreamPlayer.play()
 		Grid.move(self, event_name)
 		
