b82cf77d84557be2872a7b5e12de276ed49c54ad
Renan Batista <renfbatista@gmail.com>
2023-09-24 17:36:00 -0300

    feat: bg music

---
+++ scenes/Main/Main.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AudioStreamPlayer.play()
+	$AudioStreamPlayer.connect("finished", Callable(self, "_finished"))
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _finished():
+	$AudioStreamPlayer.play()
